c72a67e0ac2fb2788f251a9a2832cd8ec31af975
daffaarkantaqqy <daffaarkantaqqy@gmail.com>
2022-02-24 21:53:49 +0700

    Mengerjakan Latihan Di Pertemuan 2

---
+++ kuliah/pertemuan2/latihan1.php
@@ -0,0 +1,52 @@
+<?php 
+// Pertemuan 2 - PHP Dasar
+// Sintaks
+// Standar Output
+// echo, print
+// print_r
+// var_dump
+
+// Penulisan sintaks PHP
+// 1. PHP di dalam HTML
+// 2. HTML di dalam PHP
+
+// Variable dam Tipe Data
+// Variabel
+// tidak boleh diawali dengan angka, tapi boleh mengandung angka
+// $nama = "Daffa Arkantaqqy Khoisy";
+// echo 'Halo, nama saya $nama';
+
+// Operator
+// aritmatika
+// + - * / %
+// $x = 10;
+// $y = 20;
+// echo $x * $y;
+
+// penggabung string / contatenation
+// .
+// $nama_depan = "Daffa"
+// $nama_belakang = "Arkantaqqy"
+// echo $nama_depan . " " . $nama_belakang
+
+// Assigement
+// =, +=, *=, /=, %=, .= 
+// $x = "1";
+// $x -= "5";
+// echo $x;
+
+// Perbandingan
+// <, >, <=, >=, ==
+// var_dump(1 == "1");
+
+// identitas
+// var_dump(1 == "1");
+// ===, !==
+
+// Logika
+// &&, ||, !
+// $x = 10;
+// var_dump($x < 20 && $x % 2 ==0);
+
+
+?>
