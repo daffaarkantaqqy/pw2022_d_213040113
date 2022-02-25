f21f340196b5fe1cbd5e737e635fb3930886d97a
daffaarkantaqqy <daffaarkantaqqy@gmail.com>
2022-02-25 20:49:01 +0700

    Mengerjakan Latihan Di Pertemuan 3

---
+++ kuliah/pertemuan3/latihan2.php
@@ -0,0 +1,17 @@
+<?php 
+// pengkondisian
+// if else
+// if else if else
+// ternary
+// switch
+
+$x = 2;
+if( $c < 20) {
+    echo "Benar";
+} else if ( $x == 20 ) {
+    echo "Bingo!";
+} else {
+    echo "Salah";
+}
+
+?>
